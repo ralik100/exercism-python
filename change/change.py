2fc32109637c7ab69cd26c4c75ea212a8bfbd02c
SBagniewski <168017387+ralik100@users.noreply.github.com>
2024-09-01 17:52:53 +0200

    v 1.0.1

--- change/change.py
+++ change/change.py
@@ -1,2 +1,25 @@
 def find_fewest_coins(coins, target):
-    pass
+    """
+    params:
+    coins - list : value of possible change coins
+    target - int : value of change
+
+    result - list : minimum possible list containing coins needed to give change
+    """
+    res=[]
+
+
+    coin_list=[]
+    for coin in coins:
+            if coin <= target:
+                coin_list.insert(0,coin)
+
+    for i in range(coin_list):
+        
+        temp=target
+
+        while temp!=0:
+             
+
+    
+    return res
