0f3d84aa53a09980b48fb0a8358c26a4f65f7e71
SBagniewski <168017387+ralik100@users.noreply.github.com>
2024-09-09 13:13:48 +0200

    v.1.0.2

--- change/change.py
+++ change/change.py
@@ -14,12 +14,23 @@ def find_fewest_coins(coins, target):
             if coin <= target:
                 coin_list.insert(0,coin)
 
-    for i in range(coin_list):
-        
-        temp=target
-
-        while temp!=0:
-             
+    if len(coin_list)==1:
+          res.append(coin_list[0])
+          return res
+    
+    temp=target
+    while temp!=0:
+          if temp%coin_list[-1]==0:
+                for i in range(temp//coin_list[-1]):
+                      res.append(coin_list[-1])
+                break
+          if temp%coin_list[-1]!=0:
+                temp=temp-coin_list[-1]
+                res.append(coin_list[-1]) 
+                coin_list.pop(-1)
+                
 
     
     return res
+
+print(find_fewest_coins([1, 5, 10, 25], 1))
