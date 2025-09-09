cf4d852396c731cbeb2b2930f91f4f3800852512
ralik100 <sbagniewski051@gmail.com>
2025-09-09 21:45:20 +0200

    v.1.0.1

--- dominoes/dominoes.py
+++ dominoes/dominoes.py
@@ -9,21 +9,62 @@ def link(dominoes):
         linked[stone]=matches
 
     return linked
-
-def make_chain(stone, linked):
-    result=[]
-    matches=linked[stone]
-    for i in range(len(matches)):
-        
+    
 
 def can_chain(dominoes):
-    result=[]
     links=link(dominoes)
+    result=[]
+    length=len(dominoes)
+    revd=[]
+
+    if length==0:
+        return None
+    if length==1:
+        stone=dominoes[0]
+        if stone[0]==stone[1]:
+            return stone
+        else:
+            return None
     
+    result.append(dominoes.pop(0))
+    
+    
+    while dominoes:
+
+        stone=result[-1]
+        x=dominoes.pop(0)
+        if x in links[stone] or x == revd[-1] and tuple(x[1],x[0]) in links[stone]:
+            if x[0]==stone[1] :
+                result.append(x)
+            else:
+                dominoes.append(x)
+        else:
+            dominoes.append(x)
+
             
+        
+        if x in links[stone] and len(dominoes)==1:
+            x=dominoes.pop(0)
+            revd.append(x)
+            new_x=(x[1],x[0])
+            x=new_x
+            dominoes.insert(0,x)
+            links[stone].append(x)
 
-    return result
+
+        
+        if x not in links[stone]:
+
+            return None   
+
+         
+
+    if result[0][0]==result[-1][1]:
+        print("asd")
+        return result
+    else:
+        return None
 
 
-input_dominoes = [(1, 2), (4, 1), (2, 3)]
+input_dominoes = [(1, 2), (1, 3), (2, 3)]
 print(can_chain(input_dominoes))
