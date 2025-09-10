5b1c1e771eb05d36fe54281a13d2dccbef2705a3
ralik100 <sbagniewski051@gmail.com>
2025-09-10 22:15:56 +0200

    v.1.0.2

--- word_count/count_words.py
+++ word_count/count_words.py
@@ -2,20 +2,33 @@ def count_words(sentence):
     res={}
     ls=[]
     string=""
+    counter=0
     for letter in sentence:
         if letter.isalpha() or letter.isdigit() or letter=="'":
             if  (sentence[sentence.index(letter)-1]).isalpha() or letter.isalpha() or letter=="'":
+
+                try:
+                    if letter=="'" and string[-1].islower() and sentence[counter+1].islower():
+                        print(letter)
+                        print(sentence[counter+1])
+                        string+=letter
+                        continue
+                except IndexError:
+                    continue
                 string+=letter
         else:
             ls.append(string.lower())
             string=""
+        counter+=1
+
     if string!="":
         ls.append(string.lower())
     for word in ls:
+
         if word not in res and word!="" and word!="'":
             res[word]=ls.count(word)   
     return res
 
 
 
-print(count_words("'First: don't laugh. Then: don't cry. You're getting it.'"))
+print(count_words("Joe can't tell between 'large' and large."))
