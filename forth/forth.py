ba1cc2a5b250d162dcb9a6f0ecd490e03bed4a07
ralik100 <sbagniewski051@gmail.com>
2025-09-13 23:16:10 +0200

    v.1.0.2

--- forth/forth.py
+++ forth/forth.py
@@ -8,13 +8,18 @@ def math_d(strong, numb_one, numb_two):
             return numb_two*numb_one
         case "/":
             return numb_two//numb_one
+        case "dup":
+            pass
+
 
 class StackUnderflowError(Exception):
     def __init__(self, message):
         self.message=message
     
     
-    
+class MyZeroDivisionError(Exception):
+    def __init__(self, message):
+        self.message=message
 
 
 
@@ -24,29 +29,27 @@ def evaluate(input_data):
     for i in range(len(stack)):
         try:
             x=int(stack[i])
-        except AttributeError:
-            pass
-        except ValueError:
-            continue
-        if x:
             stack[i]=x
+        except (AttributeError,ValueError):
+            continue
 
-    
-    while stack:
-        item=stack.pop()
+    for i in range(len(stack)):
+
+        item=stack.pop(0)
         if type(item)==str:
             try:
-                number_one=stack.pop()
-                number_two=stack.pop()
+                number_one=stack.pop(-1)
+                number_two=stack.pop(-1)
                 stack.append(math_d(item,number_one,number_two))
             except IndexError:
                 raise StackUnderflowError("Insufficient number of items in stack")
+            except ZeroDivisionError:
+                raise ZeroDivisionError("divide by zero")
         else:
             stack.append(item)
-            break
-
     return stack
 
-#necessary to change logic to pass all tests, stack must be done from beginning of list on from back
 
-print(evaluate(["1 2 +"]))
+
+
+print(evaluate(["4 0 /"]))
