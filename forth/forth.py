31d83da3b076b9c4f3f39ea7e693a8d11082ead0
ralik100 <sbagniewski051@gmail.com>
2025-09-15 18:40:00 +0200

    v.1.0.4

--- forth/forth.py
+++ forth/forth.py
@@ -1,26 +1,38 @@
-def math_d(strong,stack):
+def math_d(strong,stack, user_command):
     match strong:
         case "+":
-            one=stack.pop(-1)
-            two=stack.pop(-1)
+            one=stack.pop()
+            two=stack.pop()
             return two+one
         case "-":
-            one=stack.pop(-1)
-            two=stack.pop(-1)
+            one=stack.pop()
+            two=stack.pop()
             return two-one
         case "*":
-            one=stack.pop(-1)
-            two=stack.pop(-1)
+            one=stack.pop()
+            two=stack.pop()
             return two*one
         case "/":
-            one=stack.pop(-1)
-            two=stack.pop(-1)
+            one=stack.pop()
+            two=stack.pop()
             return two//one
         case "dup":
             one=stack[-1]
             return one
         case "drop":
             x=stack.pop()
+        case "swap":
+            one=stack.pop()
+            two=stack.pop()
+            return one, two
+        case "over":
+            item=stack[-2]
+            return item
+        case _:
+            if strong in user_command:
+                stack.extend(user_command[strong])
+                return None
+
 
             
 
@@ -37,6 +49,16 @@ class MyZeroDivisionError(Exception):
 
 
 def evaluate(input_data):
+    user_command={}
+    if len(input_data)==2:
+        user_stack=input_data.pop(0)
+        user_stack=user_stack.split(" ")
+        user_stack.pop(0)
+        user_stack.pop()
+        user_command={
+            user_stack.pop(0):user_stack
+        }
+
     stack=input_data[0].split(" ")
 
     for i in range(len(stack)):
@@ -45,25 +67,36 @@ def evaluate(input_data):
             stack[i]=x
         except (AttributeError,ValueError):
             continue
+    
 
-    for i in range(len(stack)):
-
+    while True:
+        string_count=len(list(filter(lambda x: isinstance(x, str), stack)))
+        if string_count==0:
+            break
         item=stack.pop(0)
         if type(item)==str:
             try:
                 item=item.lower()
-                result=math_d(item,stack)
-                if result!=None:
+                result=math_d(item,stack, user_command)
+
+                if result!=None and type(result)==tuple:
+                    for number in result:
+                        stack.append(number)
+
+                elif result!=None:
                     stack.append(result)
+                
             except IndexError:
                 raise StackUnderflowError("Insufficient number of items in stack")
             except ZeroDivisionError:
                 raise ZeroDivisionError("divide by zero")
         else:
             stack.append(item)
+        
+        
     return stack
 
 
 
 
-print(evaluate(["1 drop"]))
+print(evaluate([": dup-twice dup dup ;", "1 dup-twice"]))
